fb2114c1487155e1af6884409c03481c2e85ca3e
Yuriy Susuk <yuriy.susuk@scout24.com>
2017-03-02 10:26:22 +0100

    married -> manager

--- src/test/scala/com/iuriisusuk/iuvi/ItemEncoderSpecification.scala
+++ src/test/scala/com/iuriisusuk/iuvi/ItemEncoderSpecification.scala
@@ -1,7 +1,7 @@
 package com.iuriisusuk.iuvi
 
 import com.amazonaws.services.dynamodbv2.{document => dynamodb}
-import org.scalacheck.Shapeless._
+//import org.scalacheck.Shapeless._
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Arbitrary, Properties}
@@ -14,24 +14,24 @@ object Item {
     val map = new java.util.HashMap[String, Object]()
     map.put("name", name.asInstanceOf[Object])
     map.put("age", age.asInstanceOf[Object])
-    map.put("married", married.asInstanceOf[Object])
+    map.put("manager", married.asInstanceOf[Object])
 
     dynamodb.Item.fromMap(map)
   }
 }
 
-object Specification extends Properties("ItemEncoder") {
+object ItemEncoderSpecification extends Properties("ItemEncoder") {
   import ItemEncoder._
 
 //  property("simple") = forAll { (x: String, y: Int, z: Boolean) =>
 //    encode(Person(x, y, z)) == Item(x, y, z)
 //  }
 
-//  implicit val gen: Arbitrary[Person] = Arbitrary(for {
-//    name <- arbitrary[String]
-//    age <- arbitrary[Int]
-//    married <- arbitrary[Boolean]
-//  } yield Person(name, age, married))
+  implicit val gen: Arbitrary[Person] = Arbitrary(for {
+    name <- arbitrary[String]
+    age <- arbitrary[Int]
+    married <- arbitrary[Boolean]
+  } yield Person(name, age, married))
 
   property("not sos simple") = forAll { person: Person =>
     encode(person) == Item(person.name, person.age, person.manager)
